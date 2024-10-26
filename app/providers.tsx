fbfef56007a9c9b6096596c2074ed2fad214a6e7
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-26 18:35:00 +0200

    feat: built provider

---
+++ app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ThemeProvider } from "./theme-provider";
+import { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
+}
