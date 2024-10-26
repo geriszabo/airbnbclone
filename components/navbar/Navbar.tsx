3bc327b4420ba3033055bd2b1532f19bd591cc75
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-26 15:31:12 +0200

    chore: removed unnecessary react import

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { NavbarSearch } from "./NavbarSearch";
+import { LinksDropdown } from "./LinksDropdown";
+import { DarkMode } from "./DarkMode";
+import { Logo } from "./Logo";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="border-b">
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavbarSearch />
+        <div className="flex gap-4 items-center">
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+    </nav>
+  );
+};
