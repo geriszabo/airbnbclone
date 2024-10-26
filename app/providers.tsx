ad04a91867b406c3da528aeb3a409200c93aa57c
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-26 19:57:17 +0200

    chore: added toaster to providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./theme-provider";
 import { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
