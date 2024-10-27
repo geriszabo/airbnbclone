b1f936842588c47a56b5540e5282643622740e3b
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-27 11:20:49 +0100

    chore: implemented formcontainer component

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,25 +1,53 @@
+import { FormContainer } from "@/components/form/FormContainer";
 import { FormInput } from "@/components/form/FormInput";
 import { SubmitButton } from "@/components/form/SubmitButton";
 
-const createProfileAction = async(formData: FormData) =>{
-    "use server"
+const createProfileAction = async (prevState: any, formData: FormData) => {
+  "use server";
 
-    const firstName = formData.get("firstName")
-    console.log({firstName})
-}
+  const firstName = formData.get("firstName") as string;
+  if(firstName !== "geci") {
+    return {message: "Nem geci a neved bazmeg????"}
+  }
+  return { message: "Profile Created" };
+};
 
 function CreateProfilePage() {
   return (
     <section>
-        <h1 className="text-2xl font-semibold mb-8 capitalize" >new user</h1>
-        <div className="border p-8 rounded-md max-w-lg">
-            <form action={createProfileAction}>
-                <FormInput name="firstName" type="text" label="First Name"/>
-                <SubmitButton/>
-            </form>
-        </div>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+      <div className="border p-8 rounded-md">
+        <FormContainer title="Create Profile" action={createProfileAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+            {createProfileInputFields.map((field) => (
+              <FormInput
+                key={field.name}
+                name={field.name}
+                type="text"
+                label={field.label}
+              />
+            ))}
+          </div>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
+      </div>
     </section>
-  )
+  );
 }
 
-export default CreateProfilePage
+export default CreateProfilePage;
+
+const createProfileInputFields = [
+  {
+    name: "firstName",
+    label: "First Name",
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+  },
+  {
+    name: "username",
+    label: "Username",
+  },
+];
