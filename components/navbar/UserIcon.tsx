d9ac4cd459e5df3cfa4958699d9f6b9725518565
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-27 20:26:59 +0100

    feat: added usericon to navbar

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,13 @@
 import { User } from "lucide-react"
+import { fetchProfileImage } from "@/utils/actions"
+import Image from "next/image"
 
-
-export const UserIcon = () => {
+export const UserIcon = async () => {
+  const profileImage = await fetchProfileImage()
+  console.log({profileImage})
+  if(profileImage) {
+    return <Image  src={profileImage} alt="user profile image" width={6} height={6} className="w-6 h-6 rounded-full" />
+  }
   return (
     <User size={36}  className="bg-primary text-white rounded-full" />
   )
