da1d7958f4506582a8c032d8f5ec25a360ad369c
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-29 20:01:42 +0100

    chore: added imageschema and validatefield

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,16 +1,44 @@
 import * as z from "zod";
 
 export const profileSchema = z.object({
-  firstName: z.string().min(2, {message: "first name must be at least 2 characters long"}),
-  lastName: z.string().min(2, {message: "last name must be at least 2 characters long"}),
-  username: z.string().min(2, {message: "username must be at least 2 characters long"}),
+  firstName: z
+    .string()
+    .min(2, { message: "first name must be at least 2 characters long" }),
+  lastName: z
+    .string()
+    .min(2, { message: "last name must be at least 2 characters long" }),
+  username: z
+    .string()
+    .min(2, { message: "username must be at least 2 characters long" }),
 });
 
-export function validateWithZodSchema<T>(schema:z.ZodSchema<T>, data:unknown):T {
-    const result = schema.safeParse(data);
-    if(!result.success) {
-        const errors = result.error.errors.map((error) => error.message)
-        throw new Error(errors.join(", "))
-    }
-    return result.data
+export function validateWithZodSchema<T>(
+  schema: z.ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+    throw new Error(errors.join(", "));
+  }
+  return result.data;
+}
+
+export const imageSchema = z.object({
+  image: validateFile(),
+});
+
+function validateFile() {
+  const maxUploadSize = 1024 * 1024;
+  const acceptedFileTypes = ["image/"];
+  return z
+    .instanceof(File)
+    .refine((file) => {
+      return !file || file.size <= maxUploadSize;
+    }, "File size must be less then 1 MB")
+    .refine((file) => {
+      return (
+        !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
+      );
+    }, "File must be an image");
 }
