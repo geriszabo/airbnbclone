ddf61df4658e102353dcc5fe862ba17326d05744
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-29 19:45:30 +0100

    feat: added image upload to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,22 +1,33 @@
 import { FormContainer } from "@/components/form/FormContainer";
-import { updateProfileAction, fetchProfile } from "@/utils/actions";
+import {
+  updateProfileAction,
+  fetchProfile,
+  updateProfileImageAction,
+} from "@/utils/actions";
 import { FormInput } from "@/components/form/FormInput";
 import { SubmitButton } from "@/components/form/SubmitButton";
 import { createProfileInputFields } from "./create/page";
 import { Profile } from "@prisma/client";
+import { ImageInputContainer } from "@/components/form/ImageInputContainer";
 
-type ProfileWithoutDates = Omit<Profile, 'createdAt' | 'updatedAt'>;
+type ProfileWithoutDates = Omit<Profile, "createdAt" | "updatedAt">;
 
 async function ProfilePage() {
-  const profile = await fetchProfile() as Profile ;
+  const profile = (await fetchProfile()) as Profile;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const {createdAt, updatedAt, ...restProfile} = profile as ProfileWithoutDates
+  const { createdAt, updatedAt, ...restProfile } =
+    profile as ProfileWithoutDates;
 
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">user profile</h1>
       <div className="border p-8 rounded-md">
-        {/* image input container */}
+        <ImageInputContainer
+          text="Update Profile Image"
+          image={profile.profileImage}
+          name={profile.username}
+          action={updateProfileImageAction}
+        />
         <FormContainer title="Update Profile" action={updateProfileAction}>
           <div className="grid md:grid-cols-2 gap-4 mt-4">
             {createProfileInputFields.map((field) => (
@@ -25,7 +36,9 @@ async function ProfilePage() {
                 name={field.name}
                 type="text"
                 label={field.label}
-                defaultValue={restProfile[field.name as keyof ProfileWithoutDates] }
+                defaultValue={
+                  restProfile[field.name as keyof ProfileWithoutDates]
+                }
               />
             ))}
           </div>
