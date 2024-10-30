6934073e88ef9d387d2c68daa8ce8566192119ee
Jetigeri <szabo.gergo87j@gmail.com>
2024-10-30 19:20:53 +0100

    chore: updated updateprofileimageaction action

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,6 +5,7 @@ import db from "./db";
 import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { uploadImage } from "./supabase";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -106,9 +107,27 @@ export const updateProfileAction = async (
   }
 };
 
-export const updateProfileImageAction = async (prevState: any, formData: FormData): Promise<{message: string}> =>{
-    const image = formData.get("image") as File
-    const validatedFields = validateWithZodSchema(imageSchema, {image})
-    console.log({validatedFields})
-    return {message: "Profile image updated successfully"}
-}
+export const updateProfileImageAction = async (
+    prevState: any,
+    formData: FormData
+  ) => {
+    const user = await getAuthUser();
+    try {
+      const image = formData.get("image") as File;
+      const validatedFields = validateWithZodSchema(imageSchema, { image });
+      const fullPath = await uploadImage(validatedFields.image);
+  
+      await db.profile.update({
+        where: {
+          clerkId: user.id,
+        },
+        data: {
+          profileImage: fullPath,
+        },
+      });
+      revalidatePath("/profile");
+      return { message: "Profile image updated successfully" };
+    } catch (error) {
+      return renderError(error);
+    }
+  };
