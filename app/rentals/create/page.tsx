ad6bc7a4c6d89ce6fe95a3db4510d2cd4c11e2e4
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-03 16:04:06 +0100

    feat: created amenities form input

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -1,53 +1,75 @@
-"use client"
+"use client";
 
-import { FormInput } from "@/components/form/FormInput"
-import { FormContainer } from "@/components/form/FormContainer"
-import { createPropertyAction } from "@/utils/actions"
-import { SubmitButton } from "@/components/form/SubmitButton"
-import { PriceInput } from "@/components/form/PriceInput"
-import { FormCategoriesInput } from "@/components/form/FormCategoriesInput"
-import { TextAreaInput } from "@/components/form/TextAreaInput"
-import { FormCountriesInput } from "@/components/form/FormCountriesInput"
-import { ImageInput } from "@/components/form/ImageInput"
-import { FormCounterInput } from "@/components/form/FormCounterInput"
-
+import { FormInput } from "@/components/form/FormInput";
+import { FormContainer } from "@/components/form/FormContainer";
+import { createPropertyAction } from "@/utils/actions";
+import { SubmitButton } from "@/components/form/SubmitButton";
+import { PriceInput } from "@/components/form/PriceInput";
+import { FormCategoriesInput } from "@/components/form/FormCategoriesInput";
+import { TextAreaInput } from "@/components/form/TextAreaInput";
+import { FormCountriesInput } from "@/components/form/FormCountriesInput";
+import { ImageInput } from "@/components/form/ImageInput";
+import { FormCounterInput } from "@/components/form/FormCounterInput";
+import { FormAmenitiesInput } from "@/components/form/FormAmenitiesInput";
 
 type RoomDetailsType = {
-    label: "guests" | "bedrooms" | "beds" | "baths"
-}
+  label: "guests" | "bedrooms" | "beds" | "baths";
+};
 
 const roomDetails: RoomDetailsType[] = [
-    {label: 'baths'},
-    {label: 'bedrooms'},
-    {label: 'beds'},
-    {label: 'guests'},
-]
+  { label: "baths" },
+  { label: "bedrooms" },
+  { label: "beds" },
+  { label: "guests" },
+];
 
 export default function CreatePropertyPage() {
   return (
     <section>
-        <h1 className="text-2xl font-semibold mb-8 capitalize">
-            create property
-        </h1>
-        <div className="border p-8 rounded">
-            <h3 className="text-lg mb-4 font-medium" > General Info</h3>
-            <FormContainer action={createPropertyAction} title="Create Property Page" >
-                <div className="grid md:grid-cols-2 gap-8 mb-4">
-                    <FormInput name="name" type="text" label="Name (20 limit)" defaultValue="Cabin in Austria" />
-                    <FormInput name="tagline" type="text" label="Tagline (20 limit)" defaultValue="Dream Getaway Awaits You Here" />
-                    <PriceInput/>
-                    <FormCategoriesInput/>
-                </div>
-                    <TextAreaInput name="description" labelText="Description 10-100 words)" />
-                    <div className="grid sm:grid-cols-2 gap-8 mt-4">
-                        <FormCountriesInput/>
-                        <ImageInput/>
-                    </div>
-                    <h3 className="text-lg mt-8 mb-4 font-medium" >Accomodation Details</h3>
-                    {roomDetails.map(room => <FormCounterInput key={room.label}  detail={room.label}  />)}
-                <SubmitButton text="create rental" className="mb-12"/>
-            </FormContainer>
-        </div>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">
+        create property
+      </h1>
+      <div className="border p-8 rounded">
+        <h3 className="text-lg mb-4 font-medium"> General Info</h3>
+        <FormContainer
+          action={createPropertyAction}
+          title="Create Property Page"
+        >
+          <div className="grid md:grid-cols-2 gap-8 mb-4">
+            <FormInput
+              name="name"
+              type="text"
+              label="Name (20 limit)"
+              defaultValue="Cabin in Austria"
+            />
+            <FormInput
+              name="tagline"
+              type="text"
+              label="Tagline (20 limit)"
+              defaultValue="Dream Getaway Awaits You Here"
+            />
+            <PriceInput />
+            <FormCategoriesInput />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="Description 10-100 words)"
+          />
+          <div className="grid sm:grid-cols-2 gap-8 mt-4">
+            <FormCountriesInput />
+            <ImageInput />
+          </div>
+          <h3 className="text-lg mt-8 mb-4 font-medium">
+            Accomodation Details
+          </h3>
+          {roomDetails.map((room) => (
+            <FormCounterInput key={room.label} detail={room.label} />
+          ))}
+          <h3 className="text-lg mt-10 mb-6 font-medium">Amenities</h3>
+          <FormAmenitiesInput />
+          <SubmitButton text="create rental" className="mt-12" />
+        </FormContainer>
+      </div>
     </section>
-  )
+  );
 }
---
+++ components/form/FormAmenitiesInput.tsx
@@ -0,0 +1,56 @@
+import { amenities, Amenity } from "@/utils/amenities";
+import { useState } from "react";
+import { Checkbox } from "../ui/checkbox";
+import { Label } from "../ui/label";
+
+interface FormAmenitiesInputProps {
+  defaultValue?: Amenity[];
+}
+
+export const FormAmenitiesInput = ({
+  defaultValue,
+}: FormAmenitiesInputProps) => {
+  const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(
+    defaultValue || amenities
+  );
+
+  function handleChange(amenity: Amenity) {
+    setSelectedAmenities((prevAmenity) => {
+      return prevAmenity.map((prevAmenity) => {
+        if (prevAmenity.name === amenity.name) {
+          return { ...prevAmenity, selected: !prevAmenity.selected };
+        }
+        return prevAmenity;
+      });
+    });
+  }
+
+  return (
+    <section>
+      <input
+        type="hidden"
+        name="amenities"
+        value={JSON.stringify(selectedAmenities)}
+      />
+      <div className="grid grid-cols-2 gap-4">
+        {selectedAmenities.map((amenity) => {
+          return (
+            <div className="flex items-center space-x-2" key={amenity.name}>
+              <Checkbox
+                id={amenity.name}
+                checked={amenity.selected}
+                onCheckedChange={() => handleChange(amenity)}
+              />
+              <Label
+                htmlFor={amenity.name}
+                className="font-medium text-sm leading-none capitalize flex gap-x-2 items-cnter"
+              >
+                {amenity.name} <amenity.icon className="w-4 h-4" />
+              </Label>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
