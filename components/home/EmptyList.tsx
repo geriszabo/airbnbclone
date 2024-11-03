9c4b168e6e3ef9bb58e134d4f8142d610193b23c
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-03 18:20:01 +0100

    feat: created empty list comp

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
 
-export const EmptyList = () => {
-  return (
-    <div>EmptyList</div>
-  )
+interface EmptyListProps {
+  heading?: string;
+  message?: string;
+  btnText?: string;
 }
+
+export const EmptyList = ({
+  btnText = "back home",
+  message = "Keep exploring our properties",
+  heading = "No items in the list",
+}: EmptyListProps) => {
+  return (
+    <div className="mt-4">
+      <h2 className="text-xl font-bold">{heading}</h2>
+      <p className="text-lg">{message}</p>
+      <Button asChild className="mt-4 capitalize" size="lg">
+        <Link href="/"> {btnText} </Link>
+      </Button>
+    </div>
+  );
+};
