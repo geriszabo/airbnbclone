b93a57eba1c173c82016d6377d60948481bd7b2e
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-04 21:06:02 +0100

    chore: created property list comp

--- components/home/HomePropertiesList.tsx
+++ components/home/HomePropertiesList.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { PropertyCardProps } from "@/utils/types";
+import { PropertyCard } from "../card/PropertyCard";
 
-export const HomePropertiesList = () => {
-  return (
-    <div>HomePropertiesList</div>
-  )
+interface HomePropertiesListProps {
+  properties: PropertyCardProps[];
 }
+
+export const HomePropertiesList = ({ properties }: HomePropertiesListProps) => {
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" >
+      {properties.map((property) => (
+        <PropertyCard key={property.id} {...property}/>
+      ))}
+    </section>
+  );
+};
