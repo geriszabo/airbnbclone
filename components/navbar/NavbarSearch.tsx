257428581e0b27cb9e2a58e9378eda94f1b8c483
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-04 23:19:55 +0100

    chore: implemented navbar search

--- components/navbar/NavbarSearch.tsx
+++ components/navbar/NavbarSearch.tsx
@@ -1,7 +1,44 @@
+"use client";
 
-import { Input } from "../ui/input"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export const NavbarSearch = () => {
-  return (<Input type="text" placeholder="find a property..." className="max-w-xs dark:bg-mted"/>
-  )
-}
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathName}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams]);
+
+  return (
+    <Input
+      type="text"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-mted"
+      value={search}
+      onChange={(e) => (
+        setSearch(e.target.value), handleSearch(e.target.value)
+      )}
+    />
+  );
+};
