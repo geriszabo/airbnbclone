32c0a7234945865e2c7a4a29947734897d159cf6
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-05 10:57:16 +0100

    chore: added conditional rendering to card faovrite toggle button

--- components/card/CardFavoriteToggleButton.tsx
+++ components/card/CardFavoriteToggleButton.tsx
@@ -1,15 +1,23 @@
-import { Heart } from "lucide-react"
-import { Button } from "../ui/button"
-
+import { Heart } from "lucide-react";
+import { Button } from "../ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/SubmitButton";
 
 interface CardFavoriteToggleButtonProps {
-  propertyId: string
+  propertyId: string;
 }
 
-export const CardFavoriteToggleButton = ({propertyId}: CardFavoriteToggleButtonProps) => {
+export const CardFavoriteToggleButton = async ({
+  propertyId,
+}: CardFavoriteToggleButtonProps) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return <CardSignInButton />;
+  }
+
   return (
-    <Button size="icon" variant="outline" className="p2 cursor-pointer"  >
-      <Heart/>
+    <Button size="icon" variant="outline" className="p2 cursor-pointer">
+      <Heart />
     </Button>
-  )
-}
+  );
+};
