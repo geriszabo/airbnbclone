a906559f89fe06feb7fa250e26bbae7d049404a6
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-06 19:58:27 +0100

    chore: format file

--- components/form/SubmitButton.tsx
+++ components/form/SubmitButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReloadIcon } from "@radix-ui/react-icons";
-import {  useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 import { Button, ButtonProps } from "../ui/button";
 import { SignInButton } from "@clerk/nextjs";
 import { Heart, RefreshCw } from "lucide-react";
@@ -9,13 +9,13 @@ import { Heart, RefreshCw } from "lucide-react";
 interface SubmitButtonProps {
   text?: string;
   className?: ButtonProps["className"];
-  size?: ButtonProps["size"]
+  size?: ButtonProps["size"];
 }
 
 export const SubmitButton = ({
   text = "submit",
   className = "",
-  size = "lg"
+  size = "lg",
 }: SubmitButtonProps) => {
   const { pending } = useFormStatus();
   return (
@@ -37,27 +37,41 @@ export const SubmitButton = ({
   );
 };
 
-
 export const CardSignInButton = () => {
-  return <SignInButton  mode="modal">
-    <Button type="button" size="icon" variant="outline" className="p-2 cursor-pointer" asChild >
-      <Heart fill="black" />
-    </Button>
-  </SignInButton>
-}
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <Heart />
+      </Button>
+    </SignInButton>
+  );
+};
 
-export const CardSubmitButton = ({isFavorite}: {isFavorite: boolean}) => {
-  const {pending} = useFormStatus()
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
   function renderIcon() {
-    if(!pending) {
-      return isFavorite ? <Heart fill="black" /> : <Heart/>
+    if (!pending) {
+      return isFavorite ? <Heart fill="black" /> : <Heart />;
     }
-    return <RefreshCw className="animate-spin" />
+    return <RefreshCw className="animate-spin" />;
   }
 
-  const favoriteIcon = renderIcon()
+  const favoriteIcon = renderIcon();
 
-  return <Button type="submit" size="icon" variant="outline" className="p-y2 cursor-pointer">
-    {favoriteIcon}
-  </Button>
-}
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="outline"
+      className="p-y2 cursor-pointer"
+    >
+      {favoriteIcon}
+    </Button>
+  );
+};
