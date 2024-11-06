9901c8665fdb7ab5eb5ad1163fe30a30a0cc4d64
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-06 18:33:00 +0100

    chore: adjusted page layout

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,34 +1,47 @@
- 
-
-import { fetchPropertyDetails } from "@/utils/actions"
-import { redirect } from "next/navigation"
-import { BreadCrumbs } from "../BreadCrumbs"
-import { CardFavoriteToggleButton } from "@/components/card/CardFavoriteToggleButton"
-import { ShareButton } from "../ShareButton"
+import { fetchPropertyDetails } from "@/utils/actions";
+import { redirect } from "next/navigation";
+import { BreadCrumbs } from "../BreadCrumbs";
+import { CardFavoriteToggleButton } from "@/components/card/CardFavoriteToggleButton";
+import { ShareButton } from "../ShareButton";
+import { ImageContainer } from "../ImageContainer";
+import { CardPropertyRating } from "@/components/card/CardPropertyRating";
 
 interface PropertyDetailsPageProps {
-    params: {
-        id: string
-    }
+  params: {
+    id: string;
+  };
 }
 
+async function PropertyDetailsPage({ params }: PropertyDetailsPageProps) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) {
+    redirect("/");
+  }
+  const {} = property;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          <ShareButton name={property.name} propertyId={property.id} />
+          <CardFavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <CardPropertyRating inPage propertyId={property.id} />{" "}
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
 
-async function PropertyDetailsPage ({params}: PropertyDetailsPageProps) {
-    const property = await fetchPropertyDetails(params.id)
-    if(!property) {
-        redirect("/")
-    }
-    const {} = property
-    return <section>
-        <BreadCrumbs  name={property.name}/>
-        <header className="flex justify-between items-center mt-4" >
-            <h1 className="text-4xl font-bold capitalize" >{property.tagline}</h1>
-            <div className="flex items-center gap-x-4">
-                <ShareButton name={property.name} propertyId={property.id} />
-                <CardFavoriteToggleButton propertyId={property.id} />
-            </div>
-        </header>
+        </div>
+      </section>
     </section>
+  );
 }
 
-export default PropertyDetailsPage
+export default PropertyDetailsPage;
