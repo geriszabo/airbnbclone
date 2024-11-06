57cac431dfc0b72cb7602ec9f77955e218c194b2
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-06 16:48:42 +0100

    feat: created breadcrumbs component

---
+++ app/properties/BreadCrumbs.tsx
@@ -0,0 +1,28 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+interface BreadCrumbsProps {
+  name: string;
+}
+
+export const BreadCrumbs = ({ name }: BreadCrumbsProps) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{name}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
