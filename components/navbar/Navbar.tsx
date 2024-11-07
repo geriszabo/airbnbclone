24e63ec43afd1c43247ad5ab06c765b6b10a228d
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-07 11:13:34 +0100

    fix: wrapped search in suspense to fix build error

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,13 +2,16 @@ import { NavbarSearch } from "./NavbarSearch";
 import { LinksDropdown } from "./LinksDropdown";
 import { DarkMode } from "./DarkMode";
 import { Logo } from "./Logo";
+import { Suspense } from "react";
 
 export const Navbar = () => {
   return (
     <nav className="border-b">
       <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
         <Logo />
-        <NavbarSearch />
+        <Suspense>
+          <NavbarSearch />
+        </Suspense>
         <div className="flex gap-4 items-center">
           <DarkMode />
           <LinksDropdown />
