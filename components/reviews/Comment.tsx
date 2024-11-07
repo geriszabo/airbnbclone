cb2e97106af6232d0aaeda5eb1d550f278579b13
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-07 14:58:03 +0100

    chore: created scaffolding for review components

---
+++ components/reviews/Comment.tsx
@@ -0,0 +1,7 @@
+
+
+export const Comment = () => {
+  return (
+    <div>Comment</div>
+  )
+}
---
+++ components/reviews/PropertyReviews.tsx
@@ -0,0 +1,7 @@
+
+
+export const PropertyReviews = () => {
+  return (
+    <div>PropertyReviews</div>
+  )
+}
