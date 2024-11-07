86dc4a124a2a05319614ac645d85757e1ca3f53b
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-07 21:57:27 +0100

    refactor: refactored opening of dialog

--- components/reviews/SubmitReview.tsx
+++ components/reviews/SubmitReview.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "../ui/button";
 import { FormContainer } from "../form/FormContainer";
 import { createReviewAction } from "@/utils/actions";
@@ -11,18 +13,25 @@ import {
   DialogHeader,
 } from "../ui/dialog";
 import { DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
+import { useState } from "react";
 
 interface SubmitReviewProps {
   propertyId: string;
 }
 
 export const SubmitReview = ({ propertyId }: SubmitReviewProps) => {
+  const [open, setOpen] = useState(false);
+
+  //We need this to be able to close the component from inside the FormContainer
+  function handleClose() {
+    setOpen(false);
+  }
 
   return (
     <div className="mt-8">
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-            <Button >Leave a review</Button>
+          <Button>Leave a review</Button>
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
@@ -30,18 +39,21 @@ export const SubmitReview = ({ propertyId }: SubmitReviewProps) => {
             <DialogDescription>
               Rate this property on a scale of 5, write an honest feedback.
             </DialogDescription>
-            <FormContainer title="" action={createReviewAction}>
-            <input type="hidden" name="propertyId" value={propertyId} />
-            <RatingInput name="rating" />
-            <TextAreaInput
-              name="comment"
-              labelText="your thoughts on this property"
-              defaultValue="Amazing place"
-            />
-            <SubmitButton text="Submit" className="mt-4" />
-          </FormContainer>
+            <FormContainer
+              onSuccess={handleClose}
+              title="Submit a review"
+              action={createReviewAction}
+            >
+              <input type="hidden" name="propertyId" value={propertyId} />
+              <RatingInput name="rating" />
+              <TextAreaInput
+                name="comment"
+                labelText="your thoughts on this property"
+                defaultValue="Amazing place"
+              />
+              <SubmitButton text="Submit" className="mt-4" />
+            </FormContainer>
           </DialogHeader>
-
         </DialogContent>
       </Dialog>
     </div>
