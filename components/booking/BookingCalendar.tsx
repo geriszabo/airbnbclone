9e525d716629c4ab4ace79ad6ee4ee35aa930d69
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-09 16:25:39 +0100

    fix: excluded booked days from calendar

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,23 +1,38 @@
 "use client";
 
-import { defaultSelected, generateBlockedPeriods } from "@/utils/calendar";
+import { defaultSelected, generateBlockedPeriods, generateDateRange, generateDisabledDates } from "@/utils/calendar";
 import { useEffect, useState } from "react";
 import { DateRange } from "react-day-picker";
 import { Calendar } from "../ui/calendar";
 import { useProperty } from "@/utils/store";
+import { useToast } from "@/hooks/use-toast";
 
 export const BookingCalendar = () => {
   const currentDate = new Date();
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
   const bookings = useProperty((state) => state.bookings)
-  
+  const {toast} = useToast()
+
   const blockedPeriods = generateBlockedPeriods({
     bookings, today: currentDate
   })
 
+  const unavailiableDates = generateDisabledDates(blockedPeriods)
+
   useEffect(() => {
+    const selectedRange = generateDateRange(range)
+    selectedRange.some(date => {
+      if(unavailiableDates[date]) {
+        setRange(defaultSelected)
+        toast({title: "Booking Calendar",
+          description: "Some dates are booked. Please select different dates"
+        })
+        return true
+      }
+      return false
+    })
     useProperty.setState({ range });
-  }, [range]);
+  }, [range, toast, unavailiableDates]);
   return (
     <Calendar
       mode="range"
