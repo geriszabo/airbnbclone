51484883dc00670abf489ae5f43189838bcde48a
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-10 16:37:52 +0100

    feat: added stats card

--- components/admin/StatsCard.tsx
+++ components/admin/StatsCard.tsx
@@ -1,7 +1,22 @@
+import { Card, CardHeader } from "../ui/card"
 
 
-export const StatsCard = () => {
+interface StatsCardProps {
+  title: string,
+  value: number
+}
+
+export const StatsCard = ({title, value}: StatsCardProps) => {
   return (
-    <div>StatsCard</div>
+    <Card className="bg-muted" >
+      <CardHeader className="flex flex-row justify-between items-center" >
+        <h3 className="capitalize text-3xl font-bold">
+          {title}
+        </h3>
+        <span className="text-primary text-5xl font-extrabold">
+          {value}
+        </span>
+      </CardHeader>
+    </Card>
   )
 }
