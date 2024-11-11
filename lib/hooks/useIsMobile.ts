835eb91b008beff21cbe1fbe4b3292e9b062ddf7
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-11 12:12:10 +0100

    chore: added mobile device detection hook

---
+++ lib/hooks/useIsMobile.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 900);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    console.log("resized")
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return isMobile;
+};
