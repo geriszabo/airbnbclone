da830fdbc1ba4790ee34ef902d911eaf0da85b03
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-12 18:05:16 +0100

    feat: created payment option with stripe

---
+++ app/api/payment/route.ts
@@ -0,0 +1,71 @@
+import Stripe from "stripe";
+import { type NextRequest, type NextResponse } from "next/server";
+import db from "@/utils/db";
+import { formatDate } from "@/utils/format";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export const POST = async (request: NextRequest, response: NextResponse) => {
+  const requestHeaders = new Headers(request.headers);
+  const origin = requestHeaders.get("origin");
+  const { bookingId } = await request.json();
+
+  const booking = await db.booking.findUnique({
+    where: {
+      id: bookingId,
+    },
+    include: {
+      property: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+  if (!booking) {
+    return Response.json(null, {
+      status: 404,
+      statusText: "Booking ID not found",
+    });
+  }
+  const {
+    totalNights, 
+    orderTotal,
+    checkIn,
+    checkOut,
+    property: { image, name },
+  } = booking;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      ui_mode: "embedded",
+      metadata: { bookingId: booking.id },
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: "eur",
+            product_data: {
+              name: `${name}`,
+              images: [image],
+              description: `Stay in this wonderful place for ${totalNights} nights, from ${formatDate(
+                checkIn
+              )} to ${formatDate(checkOut)}`,
+            },
+            unit_amount: orderTotal * 100
+          },
+        },
+      ],
+      mode: "payment",
+      return_url: `${origin}/api/confirm?session_id={CHECKOUT_SESSION_ID}`
+    });
+    return Response.json({clientSecret: session.client_secret})
+  } catch (error) {
+    console.log(error)
+    return Response.json(null, {
+        status: 500,
+        statusText: "Internal Server Error During Payment"
+    })
+  }
+};
--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,37 @@
+"use client";
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js";
+import { useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import {
+  EmbeddedCheckout,
+  EmbeddedCheckoutProvider,
+} from "@stripe/react-stripe-js";
 
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
 
 function CheckoutPage() {
+  const searchParams = useSearchParams();
+  const bookingId = searchParams.get("bookingId");
+
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post("/api/payment", {
+      bookingId,
+    });
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
   return (
-    <div>CheckoutPage</div>
-  )
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
 }
 
-export default CheckoutPage
+export default CheckoutPage;
